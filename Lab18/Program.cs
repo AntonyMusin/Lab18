f256c9f73bc0dcad2c64b68d25f3561057785f45
Professional <Professional@USER-PC>
2021-11-06 15:19:21 +0300

    Добавьте файлы проекта.

---
+++ Lab18/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Lab18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string brackets = "a{b[]{cd}t(0)}";
+            Stack<char> stackBrackets = new Stack<char>();
+            char[] arr = brackets.ToCharArray();
+            try
+            {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (arr[i] == '{')
+                    {
+                        stackBrackets.Push('}');
+                    }
+                    else if (arr[i] == '[')
+                    {
+                        stackBrackets.Push(']');
+                    }
+                    else if (arr[i] == '(')
+                    {
+                        stackBrackets.Push(')');
+                    }
+                    else if (arr[i] == '}')
+                    {
+                        if (stackBrackets.Peek() == '}')
+                        {
+                            stackBrackets.Pop();
+                        }
+                    }
+                    else if (arr[i] == ')')
+                    {
+                        if (stackBrackets.Peek() == ')')
+                        {
+                            stackBrackets.Pop();
+                        }
+                    }
+                    else if (arr[i] == ']')
+                    {
+                        if (stackBrackets.Peek() == ']')
+                        {
+                            stackBrackets.Pop();
+                        }
+                    }
+                }
+                if (stackBrackets.Count() > 0)
+                {
+                    Console.WriteLine("Скобки расставлены некорректно! (количество открывающих больше количества закрывающих либо использован разный тип скобок)");
+                }
+                else
+                {
+                    Console.WriteLine("Скобки расставлены корректно");
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Скобки расставлены некорректно! (количество закрывающих больше открывающих)");
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
